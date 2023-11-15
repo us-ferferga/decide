9720295e883d9aad7e55d049524c68ba164b9dc0
Fernando Fernández <ferferga@hotmail.com>
2023-11-15 19:33:51 +0000

    chore(booth/frontend): scaffold vite config
    Signed-off-by: GitHub <noreply@github.com>

---
+++ decide/booth/frontend/vite.config.ts
@@ -0,0 +1,111 @@
+import { defineConfig, UserConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
+import VueRouter from 'unplugin-vue-router/vite';
+import {
+  VueUseComponentsResolver,
+  VueUseDirectiveResolver
+} from 'unplugin-vue-components/resolvers';
+import { presetUno } from 'unocss';
+import UnoCSS from 'unocss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+
+export default defineConfig(({ mode }): UserConfig => {
+  const config: UserConfig = {
+    appType: 'spa',
+    base: './',
+    plugins: [
+      VueRouter({
+        dts: './types/global/routes.d.ts',
+        importMode: 'sync'
+      }),
+      UnoCSS({
+        presets: [
+          presetUno()
+        ]
+      }),
+      vue({
+        script: {
+          defineModel: true
+        }
+      }),
+      // This plugin allows to autoimport vue components
+      Components({
+        dts: './types/global/components.d.ts',
+        /**
+         * The icons resolver finds icons components from 'unplugin-icons' using this convenction:
+         * {prefix}-{collection}-{icon} e.g. <i-mdi-thumb-up />
+         */
+        resolvers: [
+          IconsResolver(),
+          VueUseComponentsResolver(),
+          VueUseDirectiveResolver()
+        ]
+      }),
+      /**
+       * This plugin allows to use all icons from Iconify as vue components
+       * See: https://github.com/antfu/unplugin-icons
+       */
+      Icons({
+        compiler: 'vue3'
+      })
+    ],
+    build: {
+      /**
+       * See main.ts for an explanation of this target
+       */
+      target: 'es2022',
+      cssCodeSplit: false,
+      cssMinify: 'lightningcss',
+      modulePreload: false,
+      reportCompressedSize: false,
+      rollupOptions: {
+        output: {
+          plugins: [
+            mode === 'analyze'
+              ?
+              visualizer({
+                open: true,
+                filename: 'dist/stats.html',
+                gzipSize: true,
+                brotliSize: true
+              })
+              : undefined
+          ]
+        }
+      }
+    },
+    css: {
+      lightningcss: {
+        nonStandard: {
+          deepSelectorCombinator: true
+        },
+        targets: browserslistToTargets(browserslist())
+      }
+    },
+    preview: {
+      port: 3000,
+      strictPort: true,
+      host: '0.0.0.0',
+      cors: true
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3000
+    },
+    resolve: {
+      alias: {
+        '@/': './src'
+      }
+    },
+    worker: {
+      format: 'es'
+    }
+  };
+
+  return config;
+});
