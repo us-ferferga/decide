df3e7005fea1b56b6d9e083dff97a4009ccb7b41
Ahydul <ahydul1@gmail.com>
2023-11-15 13:11:54 +0100

    feat(social-authentication): add github authentication to booth

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import GetUserView, LogoutView, RegisterView
+from .views import GetUserView, LogoutView, RegisterView, account, ObtainSocialAuthTokenView
 
 
 urlpatterns = [
     path('login/', obtain_auth_token),
+    path('login-social/', ObtainSocialAuthTokenView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterView.as_view()),
-]
+    ]
