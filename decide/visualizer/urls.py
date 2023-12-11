814d70a717461678867aa9cc70bcb39f9c9cd1a2
Carlos Varela Soult <80246400+carvarsou@users.noreply.github.com>
2023-12-11 19:13:52 +0100

    feat(data-visualizer): add two charts and statistics

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
-    path('visualizer/<int:voting_id>/graphics/', views.graphics),
+    path('visualizer/<int:voting_id>/graphics/', VisualizerView.graphics),
 ]
