b8e4792a5df0dc17857c96d085ee80ecce859651
Fernando Fernández <ferferga@hotmail.com>
2023-12-16 18:10:44 +0000

    feat(boot): create new viewset [untested]
    Signed-off-by: GitHub <noreply@github.com>

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BoothView
+from .views import BoothView, BoothApiView, PingView, NewBoothFrontendView
 
 
 urlpatterns = [
-    path('<int:voting_id>/', BoothView.as_view()),
+    path('', NewBoothFrontendView.as_view()),
+    path('api', PingView.as_view()),
+    path('api/<int:vid>/', BoothApiView.as_view()),
+    path('new/<int:voting_id>/', NewBoothFrontendView.as_view()),
+    path('old/<int:voting_id>/', BoothView.as_view())
 ]
