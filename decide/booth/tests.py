97f935025702706b9d38345aed734a21798264d4
Fernando Fernández <ferferga@hotmail.com>
2023-12-17 17:46:00 +0000

    test(backend): implement tests
    Signed-off-by: GitHub <noreply@github.com>

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -10,15 +10,19 @@ class BoothTestCase(BaseTestCase):
     def tearDown(self):
         super().tearDown()
     def testBoothNotFound(self):
-        
         # Se va a probar con el numero 10000 pues en las condiciones actuales en las que nos encontramos no parece posible que se genren 10000 votaciones diferentes
-        response = self.client.get('/booth/10000/')
+        response = self.client.get('/booth/old/10000/')
         self.assertEqual(response.status_code, 404)
     
     def testBoothRedirection(self):
-        
         # Se va a probar con el numero 10000 pues en las condiciones actuales en las que nos encontramos no parece posible que se genren 10000 votaciones diferentes
-        response = self.client.get('/booth/10000')
+        response = self.client.get('/booth/old/10000')
         self.assertEqual(response.status_code, 301)
 
-       
+    def testBoothPing(self):
+        response = self.client.get('/booth/api', format='json')
+        self.assertEqual(response.status_code, 200)
+
+    def testApiNotFound(self):
+        response = self.client.get('/booth/api/10000', format='json')
+        self.assertEqual(response.status_code, 404)
--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -5,7 +5,7 @@ from .views import BoothView, BoothApiView, PingView, NewBoothFrontendView
 urlpatterns = [
     path('', NewBoothFrontendView.as_view()),
     path('api', PingView.as_view()),
-    path('api/<int:vid>/', BoothApiView.as_view()),
+    path('api/<int:vid>', BoothApiView.as_view()),
     path('new/<int:voting_id>/', NewBoothFrontendView.as_view()),
     path('old/<int:voting_id>/', BoothView.as_view())
 ]
